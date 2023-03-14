5007ad926f645e05485f21fa78ddea1cd93d040c
Ross Cassetta <rosscassetta2014@gmail.com>
2023-03-14 13:54:14 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,48 @@
+# File Created By Ross Cassetta
+
+### IMPORTS ###
+import pygame as pg
+import random
+import os
+from settings import *
+from sprites import *
+
+### ASSETS ###
+game_folder = os.path.dirname(__file__)
+img_folder = os.path.join(game_folder, "img")
+
+def get_mouse_now():
+    x,y = pg.mouse.get_pos()
+    return (x,y)
+
+pg.init()
+pg.mixer.init()
+screen = pg.display.set_mode((WIDTH, HEIGHT))
+pg.display.set_caption("My first game...")
+clock = pg.time.Clock() 
+
+
+all_sprites = pg.sprite.Group()
+enemies = pg.sprite.Group()
+player = Player()
+
+enemy1 = Mob(80,80)
+
+all_sprites.add(player)
+all_sprites.add(enemy1)
+
+### GAME LOOP ###
+while RUNNING:
+    clock.tick(FPS)
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            RUNNING = False
+    all_sprites.update()
+
+    blocks_hit_list = pg.sprite.spritecollide(player, enemies, True)
+    for block in blocks_hit_list:
+        pass
+    screen.fill(BLUE)
+    all_sprites.draw(screen)
+    pg.display.flip()
+pg.quit()
---
+++ sprites.py
@@ -0,0 +1,96 @@
+import pygame as pg
+
+from pygame.sprite import Sprite
+
+from settings import *
+
+vec = pg.math.Vector2
+
+### PLAYER CLASS ###
+class Player(Sprite):
+    def __init__(self):
+        Sprite.__init__(self)
+        self.image = pg.Surface((50,50))
+        self.image.fill(BLACK)
+        self.rect = self.image.get_rect()
+        self.rect.center = (WIDTH/2, HEIGHT/2)
+        self.pos = vec(WIDTH/2, HEIGHT/2)
+        self.vel = vec(0,0)
+        self.acc = vec(0,0)
+        self.cofric = 0.1
+        self.canjump = False
+    def input(self):
+        keystate = pg.key.get_pressed()
+
+        if keystate[pg.K_w]:
+            self.acc.y = -PLAYER_ACC
+        if keystate[pg.K_a]:
+            self.acc.x = -PLAYER_ACC
+        if keystate[pg.K_s]:
+            self.acc.y = PLAYER_ACC
+        if keystate[pg.K_d]:
+            self.acc.x = PLAYER_ACC
+    ### BORDER ###
+    def inbounds(self):
+        if self.rect.x > WIDTH - 50:
+            self.pos.x = WIDTH - 25
+            self.vel.x = 0
+        if self.rect.x < 0:
+            self.pos.x = 25
+            self.vel.x = 0
+        if self.rect.y > HEIGHT - 50:
+            self.pos.y = HEIGHT - 25
+            self.vel.y = 0
+        if self.rect.y < 0:
+            self.pos.y = 25
+            self.vel.y = 0
+
+    def update(self):
+        self.inbounds()
+        self.acc = self.vel * PLAYER_FRICTION
+        self.input()
+        self.vel += self.acc
+        self.pos += self.vel + 0.5 * self.acc
+        self.rect.center = self.pos
+###
+class Mob(Sprite):
+    def __init__(self,width,height):
+        Sprite.__init__(self)
+        self.width = width
+        self.height = height
+        self.image = pg.Surface((self.width,self.height))
+        self.image.fill(RED)
+        self.rect = self.image.get_rect()
+        self.rect.center = (WIDTH/2, HEIGHT/2)
+        self.pos = vec(WIDTH/2, HEIGHT/2)
+        self.vel = vec(0,0)
+        self.acc = vec(0,0)
+        self.cofric = 0.1
+    # ...
+
+    def behavior(self):
+        # acc go up
+        
+        # self.acc.y = MOB_ACC
+        # self.acc.x = MOB_ACC
+        if self.rect.x > WIDTH or self.rect.x < 0 or self.rect.y < 0 or self.rect.y > HEIGHT:
+            self.vel *= -1
+        # if self.rect.x > WIDTH:
+        #     print("I'm off the right screen...")
+        # if self.rect.x < 0:
+        #     print("I'm off the left screen...")
+        # if self.rect.y < 0:
+        #     print("I'm off the top screen...")
+        #     # reduces vel
+        #     self.vel *= -1
+        #     self.acc *= -1
+        # if self.rect.y > HEIGHT:
+        #     print("I'm off the bottom screen...")
+
+    def update(self):
+        # self.inbounds()
+        self.acc = self.vel * MOB_FRICTION
+        self.behavior()
+        self.vel += self.acc
+        self.pos += self.vel + 0.5 * self.acc
+        self.rect.center = self.pos
