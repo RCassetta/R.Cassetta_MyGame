15ab3d0f84ecb3638e08f99657fb341c3fd0dea9
Ross Cassetta <rosscassetta2014@gmail.com>
2023-03-21 09:14:34 -0700

    Score + Fixed Enemies

--- main.py
+++ main.py
@@ -1,42 +1,116 @@
-# File Created By Ross Cassetta
-### IMPORTS ###
+# # File Created By Ross Cassetta
+# ### IMPORTS ###
+# import pygame as pg
+# import os
+# from settings import *
+# from sprites import *
+# from random import randint
+
+# ### ASSETS ###
+# game_folder = os.path.dirname(__file__)
+# img_folder = os.path.join(game_folder, "img")
+
+# def get_mouse_now():
+#     x,y = pg.mouse.get_pos()
+#     return (x,y)
+
+# # init pg and create window
+# pg.init()
+# # init sound mixer
+# pg.mixer.init()
+# screen = pg.display.set_mode((WIDTH, HEIGHT))
+# pg.display.set_caption("My first game...")
+# clock = pg.time.Clock() 
+
+
+# all_sprites = pg.sprite.Group()
+# enemies = pg.sprite.Group()
+# player = Player()
+# mob1 = Mob(10,10, RED)
+# all_sprites.add(player)
+# player.pos = (0,0)
+
+# print(enemies)
+# # game loop
+
+# while RUNNING:
+#     clock.tick(FPS)
+#     for event in pg.event.get():
+#         if event.type == pg.QUIT:
+#             RUNNING = False
+#     enemies.update()
+#     all_sprites.update()
+
+#     blocks_hit_list = pg.sprite.spritecollide(player, enemies, False)
+#     for block in blocks_hit_list:
+#         pass
+#     screen.fill(BLUE)
+#     all_sprites.draw(screen)
+#     pg.display.flip()
+# pg.quit()
+
+
+
+
+
+
+
+
+
+
+
+# # File Created By Ross Cassetta
+
+# ### IMPORTS ###
 import pygame as pg
 import os
 from settings import *
 from sprites import *
+from random import randint
 
-### ASSETS ###
+# ### ASSETS ###
 game_folder = os.path.dirname(__file__)
 img_folder = os.path.join(game_folder, "img")
+
+### SCORE ###
+def draw_text(text, size, color, x, y):
+    font_name = pg.font.match_font('arial')
+    font = pg.font.Font(font_name, size)
+    text_surface = font.render(text, True, color)
+    text_rect = text_surface.get_rect()
+    text_rect.midtop = (x,y)
+    screen.blit(text_surface, text_rect)
 
 def get_mouse_now():
     x,y = pg.mouse.get_pos()
     return (x,y)
 
-# init pg and create window
+### SCREEN ###
 pg.init()
-# init sound mixer
 pg.mixer.init()
 screen = pg.display.set_mode((WIDTH, HEIGHT))
 pg.display.set_caption("My first game...")
 clock = pg.time.Clock() 
 
-
+### SPRITES ###
 all_sprites = pg.sprite.Group()
 enemies = pg.sprite.Group()
+
 player = Player()
 
 all_sprites.add(player)
 player.pos = (0,0)
+
+### MOBS ###
 for i in range(0,20):
     # instantiate mobs
-    m = Mob(randint.randint(30,90), randint.randint(30, 90), (RED))
+    m = Mob(randint(30,90), randint(30, 90), (255,0,0))
+    # add enemies to enemies and all_sprites...
     enemies.add(m)
     all_sprites.add(m)
-
 print(enemies)
-# game loop
 
+### GAME ###
 while RUNNING:
     clock.tick(FPS)
     for event in pg.event.get():
@@ -44,11 +118,18 @@ while RUNNING:
             RUNNING = False
     enemies.update()
     all_sprites.update()
+    ### COLLISION ###
+    blocks_hit_list = pg.sprite.spritecollide(player, enemies, True)
+    
+    if blocks_hit_list:
+        SCORE += 1
 
-    blocks_hit_list = pg.sprite.spritecollide(player, enemies, False)
     for block in blocks_hit_list:
+        print(block)
         pass
+    
     screen.fill(BLUE)
     all_sprites.draw(screen)
+    draw_text("Score: " + str(SCORE), 22, WHITE, WIDTH/2, HEIGHT/10)
     pg.display.flip()
 pg.quit()
--- sprites.py
+++ sprites.py
@@ -1,12 +1,10 @@
 import pygame as pg
-import random as randint
 from pygame.sprite import Sprite
-
 from settings import *
-
+from random import randint
 vec = pg.math.Vector2
 
-### PLAYER CLASS ###
+### PLAYER ###
 class Player(Sprite):
     def __init__(self):
         Sprite.__init__(self)
@@ -21,7 +19,7 @@ class Player(Sprite):
         self.canjump = False
     def input(self):
         keystate = pg.key.get_pressed()
-
+    ### CONTROLS ###
         if keystate[pg.K_w]:
             self.acc.y = -PLAYER_ACC
         if keystate[pg.K_a]:
@@ -30,6 +28,10 @@ class Player(Sprite):
             self.acc.y = PLAYER_ACC
         if keystate[pg.K_d]:
             self.acc.x = PLAYER_ACC
+        if keystate[pg.K_SPACE]:
+            self.acc.x = WIDTH/2
+            self.acc.y = HEIGHT/2
+
     ### BORDER ###
     def inbounds(self):
         if self.rect.x > WIDTH - 50:
@@ -52,32 +54,36 @@ class Player(Sprite):
         self.vel += self.acc
         self.pos += self.vel + 0.5 * self.acc
         self.rect.center = self.pos
-        
+
 class Mob(Sprite):
-    def __init__(self, width, height, color):
+    def __init__(self,width,height, color):
         Sprite.__init__(self)
         self.width = width
         self.height = height
-        self.image = pg.Surface((self.width, self.height))
-        self.image.fill(color)
+        self.image = pg.Surface((self.width,self.height))
+        self.color = color
+        self.image.fill(self.color)
         self.rect = self.image.get_rect()
         self.rect.center = (WIDTH/2, HEIGHT/2)
         self.pos = vec(WIDTH/2, HEIGHT/2)
-        self.vel = vec(0,0)
-        self.acc = vec(0,0)
-        self.cofric = 0.1
-
-    def behavior(self):
-        if self.rect.x > WIDTH or self.rect.x < 0:
+        self.vel = vec(randint(1,5),randint(1,5))
+        self.acc = vec(1,1)
+        self.cofric = 0.01
+    ### BORDER ###
+    def inbounds(self):
+        if self.rect.x > WIDTH:
             self.vel.x *= -1
-        if self.rect.y < 0 or self.rect.y > HEIGHT:
+            # self.acc = self.vel * -self.cofric
+        if self.rect.x < 0:
+            self.vel.x *= -1
+            # self.acc = self.vel * -self.cofric
+        if self.rect.y < 0:
             self.vel.y *= -1
-        self.acc = vec(randint.uniform(-MOB_ACC, MOB_ACC), randint.uniform(-MOB_ACC, MOB_ACC))
-
+            # self.acc = self.vel * -self.cofric
+        if self.rect.y > HEIGHT:
+            self.vel.y *= -1
+            # self.acc = self.vel * -self.cofric
     def update(self):
-        self.behavior()
-        self.acc = self.vel * MOB_FRICTION
-        self.vel += self.acc
-        self.pos += self.vel + 0.5 * self.acc
+        self.inbounds()
+        self.pos += self.vel
         self.rect.center = self.pos
-
